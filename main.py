f49a03e5f7f69133107accdce606c47f18b8a9c6
adamklempa <adamklempa@gmail.com>
2025-08-25 18:43:32 -0500

    imported sys to main and finalized bookbot

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import sys
 from stats import get_word_count, sort_and_print_dict
 def main():
-    book_path="books/frankenstein.txt"
+    book_path = None
+    if len(sys.argv) > 1:
+        book_path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     word_count_text = f"Found {get_word_count(book_path)} total words"
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
